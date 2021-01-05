24400b1bf3accb20e84e440144f540e5a13d5b02
pcotte <pcotte@advestis.com>
2021-01-05 09:54:41 +0100

    chg: usr: removed numpy in requirements and trying to add JSONEncoder to __init__

--- transparentpath/__init__.py
+++ transparentpath/__init__.py
@@ -1,3 +1,11 @@
 from .gcsutils.transparentpath import TransparentPath
 from .gcsutils.transparentpath import TransparentPath as Path
-from transparentpath.io.json import JSONEncoder
+from .io.json import errormessage
+
+
+try:
+    from transparentpath.io.json import JSONEncoder
+except ImportError:
+    class JSONEncoder:
+        def __init__(self, *args, **kwargs):
+            raise ImportError(errormessage)
