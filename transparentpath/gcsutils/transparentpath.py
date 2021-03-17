389d866bf4f8625307641564ef73606bf897cf34
pcotte <pcotte@advestis.com>
2021-03-17 11:53:00 +0100

    new: dev: added correct cicd

--- transparentpath/gcsutils/transparentpath.py
+++ transparentpath/gcsutils/transparentpath.py
@@ -444,7 +444,6 @@ def extract_fs_name(token: str = None) -> Tuple[str, str]:
 
     fs_name = f"gcs_{content['project_id']}_{content['client_email']}"
     TransparentPath.tokens[fs_name] = token
-    print("IN EXTRACT FS NAME:", os.environ["GOOGLE_APPLICATION_CREDENTIALS"], token, content)
     TransparentPath.show_state()
     return fs_name, content["project_id"]
 
