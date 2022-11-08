2c7ca90e7d8e22fa29e7f3271c3b3fcc94f9c5c0
eguir <eguir@advestis.com>
2022-11-08 10:13:47 +0100

    chg: "cp" to "put" in get_path function in test_hdf5_ssh.py add : tests for ssh in pull-request.yml

--- tests/unit/test_hdf5_ssh.py
+++ tests/unit/test_hdf5_ssh.py
@@ -106,7 +106,7 @@ def get_path(fs_kind, use_pandas, simple):
 
     local_path = TransparentPath(f"tests/data/chien{to_add}.hdf5")
     phdf5 = TransparentPath(f"chien/chien.hdf5")
-    local_path.cp(phdf5)
+    local_path.put(phdf5)
 
     assert phdf5.is_file()
     return phdf5
