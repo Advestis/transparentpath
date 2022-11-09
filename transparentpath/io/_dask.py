27a65b38821f58e1703458606dd60a4767036e44
eguir <eguir@advestis.com>
2022-11-09 16:48:03 +0100

    try to fix bug of write_csv function

--- transparentpath/io/_dask.py
+++ transparentpath/io/_dask.py
@@ -199,7 +199,7 @@ try:
                 if not path_to_save.stem.endswith("*"):
                     path_to_save = path_to_save.parent / (path_to_save.stem + "_*.csv")
                 futures = self.__class__.cli.map(dd.to_csv, data, path_to_save.__fspath__(), **kwargs)
-                self.__class__.cli.gather(futures)
+                futures.compute()
                 TransparentPath(path=f.name, fs="local", bucket=self.bucket).put(self.path)
 
     def write_parquet(
